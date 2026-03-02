dcd96903be848c8073ded0e0b7703968f20b829d
atai <666@qq.com>
2026-03-03 07:56:05 +0800

    init_my

---
+++ util/requestsUtils/requestControl.py
@@ -0,0 +1,38 @@
+import requests
+from urllib3 import request
+
+
+class RequestControl:
+    def __init__(self) -> None:
+        # 使用session 方便以后 做登录保持
+        self.session = requests.Session()
+
+    def send_request(
+        self, method, url, headers=None, params=None, data=None, json=None, timeout=10
+    ):
+        try:
+            response = self.session.request(
+                method=method.upper(),
+                url=url,
+                headers=headers,
+                params=params,
+                data=data,
+                json=json,
+                timeout=timeout,
+            )
+        except requests.RequestException as e:
+            return {"status_code": None, "body": None, "text": str(e), "headers": None}
+        # 尝试解析 JSON
+        try:
+            body = response.json()
+        except ValueError:
+            body = response.text
+
+        # 统一返回结构
+        return {
+            "status_code": response.status_code,
+            "body": body,
+            "text": response.text, 
+            "headers": dict(response.headers)
+        }
+        
